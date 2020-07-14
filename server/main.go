984bb0c89e338cac4ab6b312a62497def0784766
Mateusz Okulewicz <mat@citrusad.com>
2020-07-14 21:36:18 +0200

    db model structure + user table

--- server/main.go
+++ server/main.go
@@ -1,49 +1,43 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
 
-  "github.com/jinzhu/gorm"
-  _ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 
-  "github.com/rungokarol/facilEspanol/login"
-
+	"github.com/rungokarol/facilEspanol/login"
+	model "github.com/rungokarol/facilEspanol/model/db"
 )
 
-type Product struct {
-  gorm.Model
-  Code string
-  Name string
-  Price uint
+func handler(responseWriter http.ResponseWriter, r *http.Request) {
+	log.Println("request received")
+
+	fmt.Fprintf(responseWriter, "Hello %s!", r.URL.Path[1:])
 }
 
-func handler(responseWriter http.ResponseWriter, r *http.Request) {
-    log.Println("request received")
+func initDb() *gorm.DB {
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=facilespanoluser dbname=facilespanoldb password=facilEspanolPass  sslmode=disable")
+	if err != nil {
+		log.Println(err)
+		panic("failed to connect database")
+	}
 
-    fmt.Fprintf(responseWriter, "Hello %s!", r.URL.Path[1:])
+    log.Println("Connected to database!")
+
+	// Migrate the schema
+	db.AutoMigrate(&model.User{})
+
+    return db
 }
 
 func main() {
-    db, err := gorm.Open("postgres", "host=localhost port=5432 user=facilespanoluser dbname=facilespanoldb password=facilEspanolPass  sslmode=disable")
-    if err != nil {
-      log.Println(err)
-      panic("failed to connect database")
-    } else {
-      log.Println("Connected to database!")
-    }
-    defer db.Close()
+    db := initDb()
+	defer db.Close()
 
-    // Migrate the schema
-    db.AutoMigrate(&Product{})
-
-    // Create
-    db.Create(&Product{Code: "L1212", Price: 1000})
-
-
-    http.HandleFunc("/", handler)
-    http.HandleFunc("/login", login.LoginHandler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/login", login.LoginHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-

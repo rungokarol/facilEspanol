d59677ebaa8940f9ddcffbd6f88190a755325e3f
Mateusz Okulewicz <mat@citrusad.com>
2020-07-14 21:18:09 +0200

    first working version

--- server/main.go
+++ server/main.go
@@ -5,8 +5,19 @@ import (
   "log"
   "net/http"
 
+  "github.com/jinzhu/gorm"
+  _ "github.com/jinzhu/gorm/dialects/postgres"
+
   "github.com/rungokarol/facilEspanol/login"
+
 )
+
+type Product struct {
+  gorm.Model
+  Code string
+  Name string
+  Price uint
+}
 
 func handler(responseWriter http.ResponseWriter, r *http.Request) {
     log.Println("request received")
@@ -15,6 +26,22 @@ func handler(responseWriter http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+    db, err := gorm.Open("postgres", "host=localhost port=5432 user=facilespanoluser dbname=facilespanoldb password=facilEspanolPass  sslmode=disable")
+    if err != nil {
+      log.Println(err)
+      panic("failed to connect database")
+    } else {
+      log.Println("Connected to database!")
+    }
+    defer db.Close()
+
+    // Migrate the schema
+    db.AutoMigrate(&Product{})
+
+    // Create
+    db.Create(&Product{Code: "L1212", Price: 1000})
+
+
     http.HandleFunc("/", handler)
     http.HandleFunc("/login", login.LoginHandler)
     log.Fatal(http.ListenAndServe(":8080", nil))

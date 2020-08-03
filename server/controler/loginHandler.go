f344e0561ccd6416b785efc2a87c5a890522fcb0
Karol Rungo <karolrungo@gmail.com>
2020-08-04 01:15:07 +0200

    move handlers into separate files

--- server/controler/loginHandler.go
+++ server/controler/loginHandler.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/rungokarol/facilEspanol/model"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -18,11 +17,6 @@ type loginReq struct {
 
 type loginResponse struct {
 	Token string `json:"token"`
-}
-
-type registerReq struct {
-	Username string
-	Password string
 }
 
 var minLength = 3
@@ -92,59 +86,3 @@ func (env *Env) Login(responseWriter http.ResponseWriter, r *http.Request) {
 	responseWriter.Write(responseJson)
 }
 
-func (env *Env) Register(responseWriter http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(responseWriter,
-			http.StatusText(http.StatusMethodNotAllowed),
-			http.StatusMethodNotAllowed)
-		return
-	}
-
-	var registerReq registerReq
-	if err := json.NewDecoder(r.Body).Decode(&registerReq); err != nil {
-		http.Error(responseWriter,
-			http.StatusText(http.StatusBadRequest),
-			http.StatusBadRequest)
-		return
-	}
-
-	if len(registerReq.Username) < minLength || len(registerReq.Password) < minLength {
-		http.Error(responseWriter,
-			"Username or password too short",
-			http.StatusBadRequest)
-		return
-	}
-
-	isPresent, err := env.store.IsUserPresent(strings.ToLower(registerReq.Username))
-	if err != nil {
-		http.Error(responseWriter,
-			http.StatusText(http.StatusInternalServerError), //check if good status maybe unauthorized?
-			http.StatusInternalServerError)
-		return
-	} else if isPresent {
-		http.Error(responseWriter,
-			"User with given username already exists",
-			http.StatusBadRequest)
-		return
-	}
-
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(registerReq.Password), bcrypt.DefaultCost)
-	if err != nil {
-		http.Error(responseWriter,
-			http.StatusText(http.StatusInternalServerError),
-			http.StatusInternalServerError)
-		return
-	}
-
-	model := model.User{
-		Username:     strings.ToLower(registerReq.Username),
-		PasswordHash: string(hashedPassword),
-	}
-
-	if err := env.store.CreateUser(&model); err != nil {
-		http.Error(responseWriter,
-			http.StatusText(http.StatusInternalServerError),
-			http.StatusInternalServerError)
-		return
-	}
-}
---
+++ server/controler/registerHandler.go
@@ -0,0 +1,72 @@
+package controler
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/rungokarol/facilEspanol/model"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type registerReq struct {
+	Username string
+	Password string
+}
+
+func (env *Env) Register(responseWriter http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(responseWriter,
+			http.StatusText(http.StatusMethodNotAllowed),
+			http.StatusMethodNotAllowed)
+		return
+	}
+
+	var registerReq registerReq
+	if err := json.NewDecoder(r.Body).Decode(&registerReq); err != nil {
+		http.Error(responseWriter,
+			http.StatusText(http.StatusBadRequest),
+			http.StatusBadRequest)
+		return
+	}
+
+	if len(registerReq.Username) < minLength || len(registerReq.Password) < minLength {
+		http.Error(responseWriter,
+			"Username or password too short",
+			http.StatusBadRequest)
+		return
+	}
+
+	isPresent, err := env.store.IsUserPresent(strings.ToLower(registerReq.Username))
+	if err != nil {
+		http.Error(responseWriter,
+			http.StatusText(http.StatusInternalServerError), //check if good status maybe unauthorized?
+			http.StatusInternalServerError)
+		return
+	} else if isPresent {
+		http.Error(responseWriter,
+			"User with given username already exists",
+			http.StatusBadRequest)
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(registerReq.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(responseWriter,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError)
+		return
+	}
+
+	model := model.User{
+		Username:     strings.ToLower(registerReq.Username),
+		PasswordHash: string(hashedPassword),
+	}
+
+	if err := env.store.CreateUser(&model); err != nil {
+		http.Error(responseWriter,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError)
+		return
+	}
+}

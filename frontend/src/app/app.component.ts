b6d0e59fbe7d7e776d05285c3530f1d849e72980
rungokarol <karolrungo@gmail.com>
2020-08-11 11:01:05 +0200

    login req error handling

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -16,10 +16,15 @@ export class AppComponent {
   constructor(private httpService: HttpService) {}
 
   loginHandler(username: string, password: string) {
-    this.httpService
-      .getToken(username, password)
-      .subscribe((data: LoginResponse) => {
+    this.httpService.getToken(username, password).subscribe({
+      next: (data: LoginResponse) => {
         this.token = data.token;
-      });
+        console.log(this.token);
+      },
+      error: (err) => console.log(err.error),
+    });
   }
 }
+
+// TODO
+// 1. unsubscribe -> is ti neccessary to keep subscription as a member and unsubscribe in ngOnDestroy?

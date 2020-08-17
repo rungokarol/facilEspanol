c9796d5a8416c2e2bef5f501988946de54729c85
rungokarol <karolrungo@gmail.com>
2020-08-17 14:20:39 +0200

    simple routing with redirection and page not found

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppMaterialModule } from './app-material/app-material.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginFormComponent],
+  declarations: [AppComponent, LoginFormComponent, PageNotFoundComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppMaterialModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],

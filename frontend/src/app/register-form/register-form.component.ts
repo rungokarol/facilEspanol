51d3b38c4415e404e40eabfc88034681270cfc2c
rungokarol <karolrungo@gmail.com>
2020-08-22 21:13:00 +0200

    reactive forms: register form with validators

---
+++ frontend/src/app/register-form/register-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.scss'],
+})
+export class RegisterFormComponent implements OnInit {
+  userData = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(3)]],
+    repeatPassword: ['', Validators.required],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  registerUser() {
+    console.log(`register`);
+  }
+
+  ngOnInit() {}
+}

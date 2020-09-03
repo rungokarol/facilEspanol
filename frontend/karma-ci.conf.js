3ead3388716f6020fe4b4e3c0dfa54d864910521
Mateusz Okulewicz <mat@citrusad.com>
2020-09-03 20:27:05 +0200

    code coverage action

--- frontend/karma-ci.conf.js
+++ frontend/karma-ci.conf.js
@@ -17,16 +17,16 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/frontend'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['html', 'lcovonly', 'text-summary', 'json-summary'],
       fixWebpackSourcePaths: true,
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    autoWatch: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
     restartOnFileChange: true,
   });
 };
